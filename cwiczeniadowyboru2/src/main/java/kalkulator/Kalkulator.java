5f779a4a7f12aedcd04ce0f1db5fea07bc3d7dd3
Anna Lubaszka <anna.lubaszka@eos-poland.pl>
2023-06-24 11:42:37 +0200

    utworzono branch2

--- cwiczeniadowyboru2/src/main/java/kalkulator/Kalkulator.java
+++ cwiczeniadowyboru2/src/main/java/kalkulator/Kalkulator.java
@@ -1,2 +1,13 @@
-package kalkulator;public class Kalkulator {
-}
+package kalkulator;
+
+import matematyka.Dodawanie;
+import matematyka.Odejmowanie;
+
+public class Kalkulator {
+    private Dodawanie dodawanie;
+    private Odejmowanie odejmowanie;
+
+    public Kalkulator() {
+        dodawanie = new Dodawanie();
+        odejmowanie = new Odejmowanie();
+    }
